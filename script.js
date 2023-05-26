94d4512e61df71c5183dd2a3ae0bf11f55614be8
Diane Poeng <95322564+dianee0@users.noreply.github.com>
2023-05-26 02:02:25 -0700

    added a bunch of icons for webpage

--- script.js
+++ script.js
@@ -1,25 +1,68 @@
-// Define the bounding box for UCSC
-var ucscBounds = [
-  [36.9770, -122.0914], // Southwest coordinates
-  [37.0029, -122.0516]  // Northeast coordinates
-];
+// Create a custom icons
+var witchIcon = L.icon({
+  iconUrl: 'img/witch.png',
+  iconSize: [32, 32], // Set the size of the icon
+  iconAnchor: [16, 32], // Set the anchor point of the icon
+});
 
-// Create a custom icon
+var pondIcon = L.icon({
+  iconUrl: 'img/pond.png',
+  iconSize: [32, 32], // Set the size of the icon
+  iconAnchor: [16, 32], // Set the anchor point of the icon
+});
+
 var caveIcon = L.icon({
   iconUrl: 'img/cave.png',
   iconSize: [32, 32], // Set the size of the icon
   iconAnchor: [16, 32], // Set the anchor point of the icon
 });
 
-var libraryIcon = L.icon({
-  iconUrl: 'img/caveplaceholder.png',
+var treehouseIcon = L.icon({
+  iconUrl: 'img/treehouse.png',
   iconSize: [32, 32], // Set the size of the icon
   iconAnchor: [16, 32], // Set the anchor point of the icon
 });
 
+var buddhaIcon = L.icon({
+  iconUrl: 'img/buddha.png',
+  iconSize: [32, 32], // Set the size of the icon
+  iconAnchor: [16, 32], // Set the anchor point of the icon
+});
+
+var wishingtreeIcon = L.icon({
+  iconUrl: 'img/wishingtree.png',
+  iconSize: [32, 32], // Set the size of the icon
+  iconAnchor: [16, 32], // Set the anchor point of the icon
+});
+
+var gardenIcon = L.icon({
+  iconUrl: 'img/garden.png',
+  iconSize: [32, 32], // Set the size of the icon
+  iconAnchor: [16, 32], // Set the anchor point of the icon 
+});
+
+var pogonipIcon = L.icon({
+  iconUrl: 'img/pond.png',
+  iconSize: [32, 32], // Set the size of the icon
+  iconAnchor: [16, 32], // Set the anchor point of the icon
+});
+
+var meadowIcon = L.icon({
+  iconUrl: 'img/meadow.png',
+  iconSize: [32, 32], // Set the size of the icon
+  iconAnchor: [16, 32], // Set the anchor point of the icon
+});
+
+var sammyIcon = L.icon({
+  iconUrl: 'img/sammy.png',
+  iconSize: [32, 32], // Set the size of the icon
+  iconAnchor: [16, 32], // Set the anchor point of the icon
+});
+
+/* end of icons */
+
 // Initialize the map and set maxBounds
 var map = L.map('map', {
-  maxBounds: ucscBounds,
   maxBoundsViscosity: 1.0, // Adjust the friction when dragging the map near the edges
 });
 
@@ -31,21 +74,54 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Add the custom image overlay to the map
 var imageUrl = "img/walkingmap.png";
-var imageBounds = ucscBounds;
+var imageBounds = [
+  [36.978216288676336, -122.07609943616285], // Southwest coordinates
+  [37.008983591650455, -122.0333580120721]  // Northeast coordinates
+];
 var imageOverlay = L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
-// Set the map view to fit the image bounds
-map.fitBounds(imageBounds);
+
+// Set the map view to center and zoom on the desired area
+map.setView([36.999, -122.056], 16);
 
 // Add a marker with the custom icon
-var locationMarker = L.marker([36.9914, -122.0609], { icon: caveIcon }).addTo(map);
-var libMarker = L.marker([37.002250, -122.053710], { icon: libraryIcon }).addTo(map);
+var witchMarker = L.marker([36.9914, -122.0609], { icon: witchIcon }).addTo(map);
+var pondMarker = L.marker([37.002250, -122.053710], { icon: pondIcon }).addTo(map);
+var caveMarker = L.marker([36.99628, -122.06876], { icon: caveIcon }).addTo(map);
 
+var treehouseMarker = L.marker([36.993931, -122.068644], { icon: treehouseIcon }).addTo(map);
+var buddhaMarker = L.marker([37.00683884024253, -122.06001471349225], { icon: buddhaIcon }).addTo(map);
+var wishingtreeMarker = L.marker([37.004641, -122.060067], { icon: wishingtreeIcon }).addTo(map);
+
+var gardenMarker = L.marker([37.024590, -122.057733], { icon: gardenIcon }).addTo(map);
+var pogonipMarker = L.marker([37.000145, -122.048138], { icon: pogonipIcon }).addTo(map);
+var meadowMarker = L.marker([36.993465199872084, -122.05966244554944], { icon: meadowIcon }).addTo(map);
+
+var sammyMarker = L.marker([36.999290100786006, -122.05981716276749], { icon: sammyIcon }).addTo(map);
+var sammyMarker2 = L.marker([36.99843848812555, -122.06706563856135], { icon: sammyIcon }).addTo(map);
 // Uncomment the following lines to show popups on marker hover
-locationMarker.on('mouseover', function(ev) {
-  locationMarker.openPopup();
-});
+witchMarker.bindTooltip("witch marker");
 
-libMarker.on('mouseover', function(ev) {
-  libMarker.openPopup();
-});
+pondMarker.bindTooltip("porter koi pond marker");
+
+caveMarker.bindTooltip("cave marker")
+
+treehouseMarker.bindTooltip("tree house");
+
+buddhaMarker.bindTooltip("buddha statues");
+
+wishingtreeMarker.bindTooltip("wishing tree marker");
+
+gardenMarker.bindTooltip("garden of eden marker");
+
+pogonipMarker.bindTooltip("pogonip koi pond marker")
+
+meadowMarker.bindTooltip("haunted meadows");
+
+sammyMarker.bindTooltip("sammy !");
+
+sammyMarker.bindTooltip("sammy !");
+
+
+  // [36.9960425, -122.07472222222222], // Southwest coordinates
+  // [37.0080556, -122.04138888888889]  // Northeast coordinates
