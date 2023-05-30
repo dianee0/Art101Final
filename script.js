3932c842a44b81b9955ee78d6f87f6d4998f189d
Diane Poeng <95322564+dianee0@users.noreply.github.com>
2023-05-29 17:47:56 -0700

    updated icons and added descriptions

--- script.js
+++ script.js
@@ -1,70 +1,75 @@
 // Create a custom icons
 var witchIcon = L.icon({
   iconUrl: 'img/witch.png',
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
+  iconSize: [64, 64], // Set the size of the icon
+  iconAnchor: [32,32], // Set the anchor point of the icon
 });
 
 var pondIcon = L.icon({
   iconUrl: 'img/pond.png',
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
+  iconSize: [64, 64], // Set the size of the icon
+  iconAnchor: [32,32], // Set the anchor point of the icon
 });
 
 var caveIcon = L.icon({
   iconUrl: 'img/cave.png',
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
+  iconSize: [64, 64], // Set the size of the icon
+  iconAnchor: [32,32], // Set the anchor point of the icon
 });
 
 var treehouseIcon = L.icon({
   iconUrl: 'img/treehouse.png',
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
+  iconSize: [64, 64], // Set the size of the icon
+  iconAnchor: [32,32], // Set the anchor point of the icon
 });
 
 var buddhaIcon = L.icon({
   iconUrl: 'img/buddha.png',
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
+  iconSize: [64, 64], // Set the size of the icon
+  iconAnchor: [32,32], // Set the anchor point of the icon
 });
 
 var wishingtreeIcon = L.icon({
   iconUrl: 'img/wishingtree.png',
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
+  iconSize: [64, 64], // Set the size of the icon
+  iconAnchor: [32,32], // Set the anchor point of the icon
 });
 
 var gardenIcon = L.icon({
   iconUrl: 'img/garden.png',
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon 
+  iconSize: [64, 64], // Set the size of the icon
+  iconAnchor: [32,32], // Set the anchor point of the icon
 });
 
 var pogonipIcon = L.icon({
   iconUrl: 'img/pond.png',
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
+  iconSize: [64, 64], // Set the size of the icon
+  iconAnchor: [32,32], // Set the anchor point of the icon
 });
 
 var meadowIcon = L.icon({
   iconUrl: 'img/meadow.png',
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
+  iconSize: [64, 64], // Set the size of the icon
+  iconAnchor: [32,32], // Set the anchor point of the icon
 });
 
 var sammyIcon = L.icon({
   iconUrl: 'img/sammy.png',
-  iconSize: [32, 32], // Set the size of the icon
-  iconAnchor: [16, 32], // Set the anchor point of the icon
+  iconSize: [64, 64], // Set the size of the icon
+  iconAnchor: [32,32], // Set the anchor point of the icon
 });
 
 /* end of icons */
 
 // Initialize the map and set maxBounds
 var map = L.map('map', {
-  maxBoundsViscosity: 1.0, // Adjust the friction when dragging the map near the edges
+  maxBoundsViscosity: 1.0,
+  center: [37.01637004536174, -122.09053059541569],
+  zoom: 14,
+  scrollWheelZoom: false, // Disable zooming with the mouse scroll wheel
 });
+
+
 
 // Add a base tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -81,8 +86,8 @@ var imageBounds = [
 var imageOverlay = L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
 
-// Set the map view to center and zoom on the desired area
-map.setView([36.999, -122.056], 16);
+// // Set the map view to center and zoom on the desired area
+// map.setView([36.999, -122.056], 16);
 
 // Add a marker with the custom icon
 var witchMarker = L.marker([36.9914, -122.0609], { icon: witchIcon }).addTo(map);
@@ -93,7 +98,7 @@ var treehouseMarker = L.marker([36.993931, -122.068644], { icon: treehouseIcon }
 var buddhaMarker = L.marker([37.00683884024253, -122.06001471349225], { icon: buddhaIcon }).addTo(map);
 var wishingtreeMarker = L.marker([37.004641, -122.060067], { icon: wishingtreeIcon }).addTo(map);
 
-var gardenMarker = L.marker([37.024590, -122.057733], { icon: gardenIcon }).addTo(map);
+var gardenMarker = L.marker([37.006625, -122.042020], { icon: gardenIcon }).addTo(map);
 var pogonipMarker = L.marker([37.000145, -122.048138], { icon: pogonipIcon }).addTo(map);
 var meadowMarker = L.marker([36.993465199872084, -122.05966244554944], { icon: meadowIcon }).addTo(map);
 
@@ -143,29 +148,82 @@ homepageButton.addEventListener('click', function() {
 });
 
 witchMarker.on('click', function() {
-  descriptionContainer.innerHTML = "<h2>Witch Location</h2><p>The witch spot is in the woods just past the Porter meadows. There are symbols drawn and carved into trees. As of right now, there is also a net swing among the trees.</p><h2>Lore</h2><p>For years, students and others have come to this spot to practice witchcraft or to simply visit and spend time in nature.</p>";
+  descriptionContainer.innerHTML = "<h2>Location</h2><p>The witch spot is in the woods just past the Porter meadows. There are symbols drawn and carved into trees. As of right now, there is also a net swing among the trees.</p><h2>Lore</h2><p>For years, students and others have come to this spot to practice witchcraft or to simply visit and spend time in nature.</p>";
   descriptionContainer.style.display = 'block'; // Show the description container
   mapContainer.style.display = 'none'; // Hide the map
   homepageButton.style.display = 'block'; // Show the "Homepage" button
 });
 
 pondMarker.on('click', function() {
-  descriptionContainer.innerHTML = "<h2>Pond Location</h2><p>This is the description of the pond location.</p>";
+  descriptionContainer.innerHTML = "<h2>Location</h2><p>The Koi Pond is located in the center of the Sesnon area at Porter college.  It is a relatively simple yet beautiful pond in the shape of a triangle with a water feature, koi fish, and various plants inside of it.  The fountain makes a peaceful sloshing sound and has an elegant yet deconstructed look to it.</p><h2>Lore</h2><p>The Porter college Instagram account has been going live weekly since May 2020 to invite viewers to witness the koi fish being fed.  An unfortunate incident occurred involving the koi pond in 2003.  A fraternity at UCSC stole one of the koi fish named Midas, or Goldie, from the pond as a part of an MTV show “The Fraternity Life.”  The koi fish tragically died as a result of the fraternity’s actions which prompted many reactions, among them being UCSC’s denial to provide resources to the fraternity from then on, including affiliation.</p>";
   descriptionContainer.style.display = 'block'; // Show the description container
   mapContainer.style.display = 'none'; // Hide the map
   homepageButton.style.display = 'block'; // Show the "Homepage" button
 });
 
 caveMarker.on('click', function() {
-  descriptionContainer.innerHTML = "<h2>Cave Location</h2><p>This is the description of the cave location.</p>";
+  descriptionContainer.innerHTML = "<h2>Location</h2><p>The Porter Cave, also known as the Empire Cave, is in the Cave Gulch area past the Porter meadows.</p><h2>Lore</h2><p> The Porter Cave is home to many interesting plants and animals, including the Dolloff Spider and the Empire Cave pseudoscorpion that have almost only been found in this cave and nowhere else.  Also, people that visit the caves have reported seeing mud sculptures left by past visitors.</p>";
   descriptionContainer.style.display = 'block'; // Show the description container
   mapContainer.style.display = 'none'; // Hide the map
   homepageButton.style.display = 'block'; // Show the "Homepage" button
 });
 
-// Add click events to other markers similarly
+treehouseMarker.on('click', function() {
+  descriptionContainer.innerHTML = "<h2>Location</h2><p>There was once a treehouse past Empire Grade in the woods outside of Porter college.</p><h2>Lore</h2><p>  It is not known who built the treehouse past the Porter Caves, but the city or the school itself had it taken down in 2016 due to dangerous encounters with the treehouse involving partying students.</p>";
+  descriptionContainer.style.display = 'block'; // Show the description container
+  mapContainer.style.display = 'none'; // Hide the map
+  homepageButton.style.display = 'block'; // Show the "Homepage" button
+});
 
-/* Put more of the functions here */
+buddhaMarker.on('click', function() {
+  descriptionContainer.innerHTML = "<h2>Location</h2><p>The Buddha Shrine is located near College 9/10, but the specific way to get there is difficult to explain.  It is in a clearing past an offshoot of the Fuel Break Road.  It is made up of manzanita trees, Buddha statues, and other items that make up a sort of fort.</p><h2>Lore</h2><p>Over time, people have left offerings, which is the intent of visiting the area, thus the shrine has taken shape as a result of this interaction with the space.  It has also been claimed that no one has stolen anything from the Buddha Shrine since it provides peace to its visitors.</p>";
+  descriptionContainer.style.display = 'block'; // Show the description container
+  mapContainer.style.display = 'none'; // Hide the map
+  homepageButton.style.display = 'block'; // Show the "Homepage" button
+});
+
+wishingtreeMarker.on('click', function() {
+  descriptionContainer.innerHTML = "<h2>Location</h2><p>The wishing tree is located near Tree 9 which is around the forest of College 9 and John R. Lewis. It is a tree that students have come to for over a decade to reveal their fears and put their wishes into the universe in hopes of them coming true.</p><h2>Lore</h2><p>Anyone can write their wish on a piece of paper, stick it on a branch of the tree, and wait patiently for the wish to come true. If you put energy into the world, it may just come back to you in a positive way. If you’re lucky, your wish may come true! This has been a tradition at UCSC for over ten years and students still go to the tree to wish upon a star, or more accurately, a branch.</p>";
+  descriptionContainer.style.display = 'block'; // Show the description container
+  mapContainer.style.display = 'none'; // Hide the map
+  homepageButton.style.display = 'block'; // Show the "Homepage" button
+});
+
+gardenMarker.on('click', function() {
+  descriptionContainer.innerHTML = "<h2>Location</h2><p>The Garden of Eden is a beautiful swimming hole and nature spot for not only UCSC students but everyone to enjoy. It consists of a river among the forest, and is the perfect place to swim, relax, and enjoy nature. It is known for being one of the most beautiful spots to visit just off of campus. It is located along the San Lorenzo River and is closest to the Crown and Merrill Colleges.</p><h2>Lore</h2><p>The Garden of Eden is known as an essential hangout spot for students at UCSC. There isn’t any crazy groundbreaking story, but the location is very important to student life.</p>";
+  descriptionContainer.style.display = 'block'; // Show the description container
+  mapContainer.style.display = 'none'; // Hide the map
+  homepageButton.style.display = 'block'; // Show the "Homepage" button
+});
+
+pogonipMarker.on('click', function() {
+  descriptionContainer.innerHTML = "<h2>Location</h2><p>The Pogonip Koi Pond is a koi pond in the middle of the forest just outside of the UCSC campus. If you hike out past the Crown and Merrill colleges, you will find a beautiful pond filled with koi fish that is perfect for a relaxing end to a fun adventure in nature.</p><h2>Lore</h2><p>The pond is known as a “bucket list item” here in Santa Cruz and is not very well known, but revered by students of UCSC.</p>";
+  descriptionContainer.style.display = 'block'; // Show the description container
+  mapContainer.style.display = 'none'; // Hide the map
+  homepageButton.style.display = 'block'; // Show the "Homepage" button
+});
+
+meadowMarker.on('click', function() {
+  descriptionContainer.innerHTML = "<h2>Location</h2><p>The Cowell Haunted Meadow is below the Cowell college near the fire road.  There have been many claims that a sort of ghost wanders this area on occasion.</p><h2>Lore</h2><p>  The ghost that has been reportedly haunting the grounds is that of Sarah Agnes Cowell.  According to the tale of her death, she passed by being hurled from her buggy due to the horse taking off unexpectedly.</p>";
+  descriptionContainer.style.display = 'block'; // Show the description container
+  mapContainer.style.display = 'none'; // Hide the map
+  homepageButton.style.display = 'block'; // Show the "Homepage" button
+});
+
+sammyMarker.on('click', function() {
+  descriptionContainer.innerHTML = "<h2>Best Banana Slug Spots</h2><p>There are a few areas around campus that ensure a higher probability of spotting banana slugs.</p><h2>Lore</h2><p>There is a strong connection between the students and their mascot the banana slug.  The history of making it the face of UCSC is an interesting one.  The administration was pushing for the sea lion to be UCSC’s mascot, but the students overwhelmingly advocated for Sammy the Slug; the students won.</p>";
+  descriptionContainer.style.display = 'block'; // Show the description container
+  mapContainer.style.display = 'none'; // Hide the map
+  homepageButton.style.display = 'block'; // Show the "Homepage" button
+});
+
+sammyMarker2.on('click', function() {
+  descriptionContainer.innerHTML = "<h2>Best Banana Slug Spots</h2><p>There are a few areas around campus that ensure a higher probability of spotting banana slugs.</p><h2>Lore</h2><p>There is a strong connection between the students and their mascot the banana slug.  The history of making it the face of UCSC is an interesting one.  The administration was pushing for the sea lion to be UCSC’s mascot, but the students overwhelmingly advocated for Sammy the Slug; the students won.</p>";
+  descriptionContainer.style.display = 'block'; // Show the description container
+  mapContainer.style.display = 'none'; // Hide the map
+  homepageButton.style.display = 'block'; // Show the "Homepage" button
+});
+
 
 // Initially hide the "Homepage" button
 homepageButton.style.display = 'none';
